d889c10bb7c0baf7eee690a77b5154a23ceecb2d
陆敏 <6000099@qq.com>
2020-04-03 15:38:20 +0800

    Py

--- TensorFlow/WorkSpase/main.py
+++ TensorFlow/WorkSpase/main.py
@@ -1,17 +1,4 @@
 #coding:utf-8
-def getText():
-    txt = open("TensorFlow/深度学习应用开发/第03讲（根据基础选修） 工欲善其事必先利其器：简明Python基础/Data/THE TRAGEDY OF ROMEO AND JULIET.txt", "r").read()
-    txt = txt.lower()
-    for ch in '!"#$%&()*+,-./:;<=>?@[\\]^_‘{|}~':
-        txt = txt.replace(ch, " ")
-    return txt
-rjTxt = getText()
-words  = rjTxt.split()
-counts = {}
-for word in words:
-    counts[word] = counts.get(word,0) + 1
-items = list(counts.items())
-items.sort(key=lambda x:x[1], reverse=True)
-for i in range(10):
-    word, count = items[i]
-    print "({},{})".format(count, word)
+import tensorflow.compat.v1 as tf
+tf.disable_eager_execution() #关闭eager运算
+print(tf.__version__)
